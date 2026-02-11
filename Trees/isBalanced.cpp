c085ee21fca92c11c9dd534d87f31220e54ca26a
Anubhav Pandey <114432545+anubhav2415@users.noreply.github.com>
2026-02-11 15:47:31 +0530

    Added isBalanced problem - Day 1

---
+++ Trees/isBalanced.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int height(TreeNode* root){
+        if(root==NULL) return 0;
+
+        int l=height(root->left);
+        if(l==-1) return -1;
+
+        int r=height(root->right);
+        if(r==-1) return -1;
+
+        if(abs(l-r)>1)
+            return -1;
+
+        return 1+max(l,r);
+    }
+    bool isBalanced(TreeNode* root) {
+        return height(root) !=-1;
+    }
+};
